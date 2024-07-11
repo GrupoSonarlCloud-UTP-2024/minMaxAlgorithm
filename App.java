1eaa6582c62659da4611bca8ddd06d1df8ffbe92
CeRoDV <75039724+CeRoDV@users.noreply.github.com>
2024-07-11 10:17:14 -0500

    Update App.java

--- App.java
+++ App.java
@@ -1,94 +1,74 @@
 import java.util.Scanner;
-import java.util.Random;
 
-public class App
-{
-    //Game Data
+public class App {
+    // Game Data
     static char[][] squares = new char[4][4];
-    static int k = 4;
+    static int k = 4, depth = 6;
     static boolean gameCompleted = false, aiTurn = false;
     static char ai, opponent;
 
-    //AI Calculations
+    // AI Calculations
     static boolean drew = false;
     static int tempWins, tempLosses, tempDraws;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int round = 1, choice;
         int[] move = new int[2];
-        for(int x = 0; x < squares.length; x++)
-        {
-            for(int y = 0; y < squares[0].length; y++)
-            {
+
+        for (int x = 0; x < squares.length; x++) {
+            for (int y = 0; y < squares[0].length; y++) {
                 squares[x][y] = '-';
             }
         }
-        
 
         printBoard();
 
         System.out.println(k + "-in-a-row, " + squares[0].length + "x" + squares.length);
 
-        //Opening Phase
-        System.out.println("Input 0 for ai to start, 1 for you");
+        // Opening Phase
+        System.out.println("Input 0 for AI to start, 1 for you");
         choice = sc.nextInt();
-        if(choice == 0)
-        {
+        if (choice == 0) {
             ai = 'x';
             opponent = 'o';
-            move = Choose(16);
+            move = Choose(depth);
             System.out.println("Move: " + move[1] + " " + move[0]);
             squares[move[0]][move[1]] = ai;
             round += 1;
 
             printBoard();
-        }
-        else
-        {
+        } else {
             ai = 'o';
             opponent = 'x';
         }
 
-        //Game Loop
-        while(!gameCompleted)
-        {
+        // Game Loop
+        while (!gameCompleted) {
             System.out.println("Round: " + round);
-            if(aiTurn)
-            {
-                System.out.println();
-                move = Choose(16);
+            if (aiTurn) {
+                move = Choose(depth);
 
                 System.out.println("Move: " + move[1] + " " + move[0]);
 
                 squares[move[0]][move[1]] = ai;
-            }
-            else
-            {
-                System.out.println("Choose Move[x][-]: ");
-                move[1] = sc.nextInt();
-                System.out.println("Choose Move[-][y]: ");
-                move[0] = sc.nextInt();
+            } else {
+                do {
+                    System.out.println("Choose Move[x][-]: ");
+                    move[1] = sc.nextInt();
+                    System.out.println("Choose Move[-][y]: ");
+                    move[0] = sc.nextInt();
+                } while (!isValidMove(move[0], move[1]));
 
                 squares[move[0]][move[1]] = opponent;
             }
 
-            int eval = Evaluation(squares, 0);
-            if(eval == 5000 || eval == -5000)
-            {
+            int eval = Evaluation(squares);
+            if (Math.abs(eval) == 5000) {
                 gameCompleted = true;
             }
 
-            if(aiTurn)
-            {
-                aiTurn = false;
-            }
-            else
-            {
-                aiTurn = true;
-            }
-
+            aiTurn = !aiTurn;
             round += 1;
 
             printBoard();
@@ -100,63 +80,33 @@ public class App
         sc.close();
     }
 
-    static void printBoard()
-    {
-        for(int x = 0; x < squares.length; x++)
-        {
+    static void printBoard() {
+        for (int x = 0; x < squares.length; x++) {
             System.out.print("| ");
-            for(int y = 0; y < squares[0].length; y++)
-            {
+            for (int y = 0; y < squares[0].length; y++) {
                 System.out.print(squares[x][y] + " | ");
             }
             System.out.println();
         }
     }
-    
-    //Returns move
-    static int[] Choose(int depth)
-    {
-        int[] candidate = {0, 0};
-        int value = 0, tempValue, result;
 
-        for(int x = 0; x < squares.length; x++)
-        {
-            for(int y = 0; y < squares[0].length; y++)
-            {
-                result = 2;
-                if(x == 0 && y == 0)
-                {
-                    candidate[0] = x;
-                    candidate[1] = y;
-                }
+    static boolean isValidMove(int x, int y) {
+        return x >= 0 && x < squares.length && y >= 0 && y < squares[0].length && squares[x][y] == '-';
+    }
 
-                if(Available(squares, x, y))
-                {
-                    char[][] board = new char[squares.length][squares[0].length];
+    static int[] Choose(int depth) {
+        int[] candidate = new int[2];
+        int bestValue = Integer.MIN_VALUE;
 
-                    for(int z = 0; z < squares.length; z++)
-                    {
-                        for(int w = 0; w < squares.length; w++)
-                        {
-                            board[z][w] = squares[z][w];
-                        }
-                    }
+        for (int x = 0; x < squares.length; x++) {
+            for (int y = 0; y < squares[0].length; y++) {
+                if (Available(squares, x, y)) {
+                    squares[x][y] = ai;
+                    int value = Search(squares, depth - 1, false);
+                    squares[x][y] = '-';
 
-                    board[x][y] = ai;
-                    tempValue = Search(board, depth, opponent);
-                    
-                    result = Compare(value, tempValue);
-                    
-                    if(result == 2)
-                    {
-                        //Random between the two nodes if they're of equal value
-                        Random rand = new Random();
-
-                        result = rand.nextInt(2);
-                    }
-
-                    if(result == 1)
-                    {
+                    if (value > bestValue) {
+                        bestValue = value;
                         candidate[0] = x;
                         candidate[1] = y;
                     }
@@ -167,302 +117,90 @@ public class App
         return candidate;
     }
 
-    //Compares two values
-    static int Compare(int value1, int value2)
-    {
-        int toReturn = 2;
-        if(value1 > value2)
-        {
-            toReturn = 0;
-        }
-        else if(value2 > value1)
-        {
-            toReturn = 1;
+    static int Search(char[][] board, int depth, boolean maximizingPlayer) {
+        int eval = Evaluation(board);
+        if (Math.abs(eval) == 5000 || depth == 0) {
+            return eval;
         }
 
-        return toReturn;
-    }
-
-    //Returns value of an option
-    static int Search(char[][] board, int depth, char turn)
-    {
-        int value, tempValue;
-
-        value = Evaluation(board, depth);
-        if(!drew && depth > 0)
-        {
-            value = 0;
-            
-            for(int x = 0; x < board.length; x++)
-            {
-                for(int y = 0; y < board[0].length; y++)
-                {
-                    if(Available(board, x, y))
-                    {
-                        char[][] testBoard = new char[board.length][board[0].length];
-
-                        for(int z = 0; z < board.length; z++)
-                        {
-                            for(int w = 0; w < board.length; w++)
-                            {
-                                testBoard[z][w] = board[z][w];
-                            }
-                        }
-
-                        testBoard[x][y] = turn;
-                        if(turn == ai)
-                        {
-                            tempValue = Search(testBoard, depth - 1, opponent);
-                            //Minimaxing - Choosing the value for the father node as the ai
-                            if((x == 0 && y == 0) || tempValue < value)
-                            {
-                                value = tempValue;
-                            }
-                        }
-                        else
-                        {
-                            tempValue = Search(testBoard, depth - 1, ai);
-                            //Minimaxing - Choosing the value for the father node as the opponent
-                            if((x == 0 && y == 0) || tempValue > value)
-                            {
-                                value = tempValue;
-                            }
-                        }
+        if (maximizingPlayer) {
+            int maxEval = Integer.MIN_VALUE;
+            for (int x = 0; x < board.length; x++) {
+                for (int y = 0; y < board[0].length; y++) {
+                    if (Available(board, x, y)) {
+                        board[x][y] = ai;
+                        int evalValue = Search(board, depth - 1, false);
+                        board[x][y] = '-';
+                        maxEval = Math.max(maxEval, evalValue);
                     }
                 }
             }
-        }
-        return value;
-    }
-
-    //Checks if the move is available
-    static boolean Available(char[][] board, int x, int y)
-    {
-        boolean availability = false;
-        if(board[x][y] == '-')
-        {
-            availability = true;
-        }
-
-        return availability;
-    }
-
-    //Returns the value of a board state; win, loss, draw or heuristics
-    static int Evaluation(char[][] board, int depth)
-    {
-
-        boolean win = false, lose = false, draw = false;
-        int rowWin = 0, rowLose = 0, maxRowWin = 0, maxRowLose = 0, value = 0;
-
-        for(int x = 0; x < board.length;x++)
-        {
-            for(int y = 0; y < board[0].length; y++)
-            {
-                if(board[x][y] == ai)
-                {
-                    rowWin++;
-                    for(int z = 1; x + z < board.length; z++)
-                    {
-                        if(board[x + z][y] == ai)
-                        {
-                            rowWin += 1;
-                        }
-                        else
-                        {
-                            if(z >= k - 1)
-                            {
-                                win = true;
-                            }
-                            break;
-                        }
+            return maxEval;
+        } else {
+            int minEval = Integer.MAX_VALUE;
+            for (int x = 0; x < board.length; x++) {
+                for (int y = 0; y < board[0].length; y++) {
+                    if (Available(board, x, y)) {
+                        board[x][y] = opponent;
+                        int evalValue = Search(board, depth - 1, true);
+                        board[x][y] = '-';
+                        minEval = Math.min(minEval, evalValue);
                     }
-
-                    for(int z = 1; y + z < board[0].length; z++)
-                    {
-                        if(board[x][y + z] == ai)
-                        {
-                            rowWin += 1;
-                        }
-                        else
-                        {
-                            if(z >= k - 1)
-                            {
-                                win = true;
-                            }
-                            break;
-                        }
-                    }
-
-                    for(int z = 1; x + z < board.length & y + z < board[0].length; z++)
-                    {
-                        if(board[x + z][y] == ai && board[x][y + z] == ai)
-                        {
-                            rowWin += 1;
-                        }
-                        else
-                        {
-                            if(z >= k - 1)
-                            {
-                                win = true;
-                            }
-                            break;
-                        }
-                    }
-
-                    for(int z = 1; x + z < board.length & y - z >= 0; z++)
-                    {
-                        if(board[x + z][y] == ai && board[x][y - z] == ai)
-                        {
-                            rowWin += 1;
-                        }
-                        else
-                        {
-                            if(z >= k - 1)
-                            {
-                                win = true;
-                            }
-                            break;
-                        }
-                    }
-
-                    if(rowWin > maxRowWin)
-                    {
-                        maxRowWin = rowWin;
-                    }
-
-                    rowWin = 0;
                 }
-                else if(board[x][y] == opponent)
-                {
-                    rowLose++;
-                    for(int z = 1; x + z < board.length; z++)
-                    {
-                        if(board[x + z][y] == opponent)
-                        {
-                            rowLose += 1;
-                        }
-                        else
-                        {
-                            if(z >= k - 1)
-                            {
-                                lose = true;
-                            }
-                            break;
-                        }
-                    }
+            }
+            return minEval;
+        }
+    }
 
-                    for(int z = 1; y + z < board[0].length; z++)
-                    {
-                        if(board[x][y + z] == opponent)
-                        {
-                            rowLose += 1;
-                        }
-                        else
-                        {
-                            if(z >= k - 1)
-                            {
-                                lose = true;
-                            }
-                            break;
-                        }
-                    }
+    static boolean Available(char[][] board, int x, int y) {
+        return board[x][y] == '-';
+    }
 
-                    for(int z = 1; x + z < board.length & y + z < board[0].length; z++)
-                    {
-                        if(board[x + z][y] == ai && board[x][y + z] == opponent)
-                        {
-                            rowLose += 1;
-                        }
-                        else
-                        {
-                            if(z >= k - 1)
-                            {
-                                lose = true;
-                            }
-                            break;
-                        }
-                    }
+    static int Evaluation(char[][] board) {
+        int aiScore = 0, opponentScore = 0;
 
-                    for(int z = 1; x + z < board.length & y - z >= 0; z++)
-                    {
-                        if(board[x + z][y] == opponent && board[x][y - z] == opponent)
-                        {
-                            rowLose += 1;
-                        }
-                        else
-                        {
-                            if(z >= k - 1)
-                            {
-                                lose = true;
-                            }
-                            break;
-                        }
-                    }
-
-                    if(rowLose > maxRowLose)
-                    {
-                        maxRowLose = rowLose;
-                    }
-                    rowLose = 0;
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board[0].length; y++) {
+                if (board[x][y] == ai) {
+                    aiScore += checkLines(board, x, y, ai);
+                } else if (board[x][y] == opponent) {
+                    opponentScore += checkLines(board, x, y, opponent);
                 }
             }
         }
 
-        if(win)
-        {
-            drew = false;
-            tempWins += 1;
-            value = 5000 + depth;
-        }
-        else if(lose)
-        {
-            drew = false;
-            tempLosses += 1;
-            value = -5000 + depth;
-        }
-        else
-        {
-            draw = true;
-            for(int x = 0; x < board.length; x++)
-            {
-                for(int y = 0; y < board[0].length; y++)
-                {
-                    if(board[x][y] == '-')
-                    {
-                        draw = false;
-                    }
-                }
-            }
+        if (aiScore >= 5000) return 5000;
+        if (opponentScore >= 5000) return -5000;
 
-            if(draw)
-            {
-                drew = true;
-                tempDraws += 1;
-                value = 0;
-            }
-            else
-            {
-                value = maxRowWin - maxRowLose;
-            }
-        }
-
-        //printTemporalBoard(board);
-        //System.out.println();
-        //System.out.println("Value: " + value);
-        //System.out.println();
-        return value;
+        return aiScore - opponentScore;
     }
 
-    static void printTemporalBoard(char[][] board)
-    {
-        for(int x = 0; x < board.length; x++)
-        {
-            System.out.print("| ");
-            for(int y = 0; y < board[0].length; y++)
-            {
-                System.out.print(board[x][y] + " | ");
+    static int checkLines(char[][] board, int x, int y, char player) {
+        int score = 0;
+
+        score += checkDirection(board, x, y, 1, 0, player); // Horizontal
+        score += checkDirection(board, x, y, 0, 1, player); // Vertical
+        score += checkDirection(board, x, y, 1, 1, player); // Diagonal \
+        score += checkDirection(board, x, y, 1, -1, player); // Diagonal /
+
+        return score;
+    }
+
+    static int checkDirection(char[][] board, int x, int y, int dx, int dy, char player) {
+        int count = 0;
+
+        for (int i = 0; i < k; i++) {
+            int newX = x + i * dx;
+            int newY = y + i * dy;
+
+            if (newX >= 0 && newX < board.length && newY >= 0 && newY < board[0].length && board[newX][newY] == player) {
+                count++;
+            } else {
+                break;
             }
-            System.out.println();
         }
+
+        if (count == k) return 5000;
+        return count;
     }
 }
